fcb1d725f665682491b6a60108a130559e40b262
Floby <odysseus2003@hotmail.com>
2010-04-08 00:56:28 +0200

    kikoo

---
+++ src/main.cc
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cc
+ *
+ *    Description:  the basic program launcher
+ *
+ *        Version:  1.0
+ *        Created:  08/04/2010 00:50:49
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Florent Jaby (FJ), florent.jaby@gmail.com
+ *        Company:  Florent Jaby
+ *
+ * =====================================================================================
+ */
+
+
